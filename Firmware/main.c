35b7b74d4fc791ed974e3b1283c75d0da5fe20ff
Micah Tritscher <micah.tritscher@gmail.com>
2025-08-24 13:57:55 +1200

    Added firmware folder

---
+++ Firmware/main.c
@@ -0,0 +1,11 @@
+
+
+#define HDC1080_TEMPERATURE         0x00
+#define HDC1080_HUMIDTY             0x01
+#define HDC1080_CONFIGURATION       0x02
+#define HDC1080_SERIAL_ID_FIRST     0xFB
+#define HDC1080_SERIAL_ID_MID       0xFC
+#define HDC1080_SERIAL_ID_LAST      0xFD
+#define HDC1080_MANUFACTURER_ID     0xFE
+#define HDC1080_DEVICE_ID           0xFF
+#define HDC1080_RESET               
